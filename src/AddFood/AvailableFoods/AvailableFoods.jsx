29c3fd747cfe14fdcbfffae88d530be2ce42c352
saad7mahmud <saad7mahmud@gmail.com>
2023-11-09 11:59:41 +0600

    search functionality added

--- src/AddFood/AvailableFoods/AvailableFoods.jsx
+++ src/AddFood/AvailableFoods/AvailableFoods.jsx
@@ -3,19 +3,26 @@ import AvailableFoodsCard from "./AvailableFoodsCard";
 import { Helmet } from "react-helmet";
 import { useState } from "react";
 import { useEffect } from "react";
+import { useRef } from "react";
+import Swal from "sweetalert2";
 
 const AvailableFoods = () => {
   // const availableFoods = useLoaderData();
   const [availableFoods, setAvailableFoods] = useState([]);
   const [asc, setAsc] = useState(true);
+  const [search, setSearch] = useState("");
+  const searchRef = useRef(null);
 
   useEffect(() => {
-    fetch(`http://localhost:5000/available-foods?sort=${asc ? "asc" : "desc"}`)
+    fetch(
+      `http://localhost:5000/available-foods?search=${search}&sort=${
+        asc ? "asc" : "desc"
+      }`
+    )
       .then((res) => res.json())
       .then((data) => setAvailableFoods(data));
-  }, [asc]);
+  }, [asc, search]);
 
-  console.log(availableFoods);
   return (
     <div>
       <Helmet>
@@ -31,6 +38,51 @@ const AvailableFoods = () => {
       <p className="text-center">
         Sorted by: {asc ? "Near Expiry Date" : "Later Expiry Date"}
       </p>
+      <div className=" justify-center m-10 flex">
+        <div className="form-control">
+          <div className="input-group">
+            <input
+              onChange={() => {
+                setSearch(searchRef.current.value);
+              }}
+              ref={searchRef}
+              type="text"
+              placeholder="Search…"
+              className="input input-bordered"
+            />
+            <button
+              onClick={() => {
+                {
+                  searchRef.current.value === "" &&
+                    Swal.fire({
+                      icon: "error",
+                      title: "Search Box Empty",
+                      text: "Please write something on search box",
+                    });
+                }
+                setSearch(searchRef.current.value);
+              }}
+              className="btn btn-square"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                />
+              </svg>
+            </button>
+          </div>
+          <p className="m-5 text-center">Searching: {search}</p>
+        </div>
+      </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 m-10 gap-5">
         {availableFoods.map((availableFood) => (
           <AvailableFoodsCard
