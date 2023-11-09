c4f672de432c79f1ea77a2787f1287f0a8e41f66
saad7mahmud <saad7mahmud@gmail.com>
2023-11-09 10:13:48 +0600

    react table added

--- src/ManageFood/ManageMyFoods.jsx
+++ src/ManageFood/ManageMyFoods.jsx
@@ -1,8 +1,10 @@
-import { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../provider/AuthProvider";
 import { Link } from "react-router-dom";
 import Swal from "sweetalert2";
 import { Helmet } from "react-helmet";
+import { useTable } from "react-table";
+
 const ManageMyFoods = () => {
   const [emailBasedDonatedFoodsPrev, setEmailBasedDonatedFoodsPrev] = useState(
     []
@@ -10,30 +12,6 @@ const ManageMyFoods = () => {
   const [emailBasedDonatedFoods, setEmailBasedDonatedFoods] = useState(
     emailBasedDonatedFoodsPrev
   );
-
-  console.log(emailBasedDonatedFoodsPrev);
-
-  const {
-    _id,
-    foodName,
-    foodImage,
-    foodQuantityNumber,
-    foodExpiryDate,
-    expiryDateMs,
-    foodPickUpLocation,
-    foodStatus,
-    additionalDonatorNotes,
-    donatorName,
-    donatorEmail,
-    donatorImage,
-  } = emailBasedDonatedFoods;
-
-  console.log(emailBasedDonatedFoods);
-
-  const handleManageFood = (id) => {
-    console.log("clicked", id);
-    
-  };
 
   const { user } = useContext(AuthContext);
   const url = `http://localhost:5000/available-foods?donatorEmail=${user?.email}`;
@@ -46,9 +24,7 @@ const ManageMyFoods = () => {
       });
   }, [url]);
 
-  // Delete Food
   const handleDelete = (id) => {
-    console.log(id);
     Swal.fire({
       title: "Are you sure?",
       text: "You won't be able to revert this!",
@@ -64,7 +40,6 @@ const ManageMyFoods = () => {
         })
           .then((res) => res.json())
           .then((data) => {
-            console.log(data);
             if (data.deletedCount > 0) {
               Swal.fire({
                 title: "Deleted!",
@@ -82,87 +57,116 @@ const ManageMyFoods = () => {
     });
   };
 
+  const data = React.useMemo(
+    () => emailBasedDonatedFoods,
+    [emailBasedDonatedFoods]
+  );
+
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: "Food Name",
+        accessor: "foodName",
+      },
+      {
+        Header: "Food Image",
+        accessor: "foodImage",
+        Cell: ({ value }) => (
+          <img
+            src={value}
+            alt="Food"
+            style={{ width: "50px", height: "50px" }}
+          />
+        ),
+      },
+      {
+        Header: "Donator Email",
+        accessor: "donatorEmail",
+      },
+      {
+        Header: "Location",
+        accessor: "foodPickUpLocation",
+      },
+      {
+        Header: "Expiry Date",
+        accessor: "foodExpiryDate",
+      },
+      {
+        Header: "Actions",
+        accessor: "_id",
+        Cell: ({ value }) => (
+          <div className="flex justify-center space-x-2">
+            <Link to={`/update-food/${value}`}>
+              <button
+                className="edit-button hover:cursor-pointer    mx-auto select-none rounded-lg bg-gradient-to-tr from-[#32a374] to-[#57b38d] py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-green-500/20 transition-all hover:shadow-lg hover:shadow-green-500/40 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                onClick={() => handleManageFood(value)}
+              >
+                Edit
+              </button>
+            </Link>
+            <button
+              className="delete-button  hover:cursor-pointer   mx-auto  select-none rounded-lg bg-gradient-to-tr from-[#ec5252] to-[#cf4242] py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-red-500/20 transition-all hover:shadow-lg hover:shadow-red-500/40 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+              onClick={() => handleDelete(value)}
+            >
+              Delete
+            </button>
+            <Link to={`/manage-my-food/${value}`}>
+              <button
+                className="manage-button hover:cursor-pointer   mx-auto select-none rounded-lg bg-gradient-to-tr from-[#327fa3] to-[#57a1b3] py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                onClick={() => handleManageFood(value)}
+              >
+                Manage
+              </button>
+            </Link>
+          </div>
+        ),
+      },
+    ],
+    []
+  );
+
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable({ columns, data });
+
   return (
-    <div>
+    <div className="mx-2 sm:mx-4 md:mx-8 lg:mx-10 xl:mx-16 2xl:mx-20">
       <Helmet>
         <title>ZeroHunger | Manage My Foods</title>
       </Helmet>
       <div>
-        <h1 className="mt-10 font-bold text-4xl text-center">
+        <h1 className="mt-6 sm:mt-10 font-bold text-2xl sm:text-4xl text-center">
           My Donated Foods
         </h1>
-        <h1 className="m-4  text-center">Email: {user?.email}</h1>
-        {emailBasedDonatedFoods.map((emailBasedDonatedFood) => (
-          <div key={emailBasedDonatedFood._id}>
-            <div className="m-10">
-              <table className="table">
-                <tbody>
-                  <tr>
-                    <td>
-                      <div className="ms-10 flex items-center space-x-3">
-                        <div className="avatar">
-                          <div className="mask mask-squircle w-24 ">
-                            <img
-                              src={emailBasedDonatedFood.foodImage}
-                              alt="Avatar Tailwind CSS Component"
-                            />
-                          </div>
-                        </div>
-                        <div className="">
-                          <div className="font-semibold my-2 text-3xl">
-                            {emailBasedDonatedFood.foodName}
-                          </div>
-                          <div className="text-md opacity-50">
-                            Food Id: {emailBasedDonatedFood._id}
-                          </div>
-                          <div className="text-md opacity-50">
-                            Location: {emailBasedDonatedFood.foodPickUpLocation}
-                          </div>
-                          <div className="text-md opacity-50">
-                            Expiry Date: {emailBasedDonatedFood.foodExpiryDate}
-                          </div>
-                          <div className="text-md opacity-50">
-                            Donator Email: {emailBasedDonatedFood.donatorEmail}
-                          </div>
-                        </div>
-                      </div>
-                    </td>
-
-                    <th>
-                      <Link
-                        className="m-2"
-                        to={`/update-food/${emailBasedDonatedFood._id}`}
-                      >
-                        <button className="hover:cursor-pointer    mx-auto select-none rounded-lg bg-gradient-to-tr from-[#32a374] to-[#57b38d] py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-green-500/20 transition-all hover:shadow-lg hover:shadow-green-500/40 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
-                          Edit
-                        </button>
-                      </Link>
-                      <button
-                        onClick={() => handleDelete(emailBasedDonatedFood._id)}
-                        className="m-2 hover:cursor-pointer   mx-auto  select-none rounded-lg bg-gradient-to-tr from-[#ec5252] to-[#cf4242] py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-red-500/20 transition-all hover:shadow-lg hover:shadow-red-500/40 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-                      >
-                        Delete
-                      </button>
-                      <Link
-                        className="m-2"
-                        to={`/manage-my-food/${emailBasedDonatedFood._id}`}
-                      >
-                        <button
-                          onClick={() =>
-                            handleManageFood(emailBasedDonatedFood._id)
-                          }
-                          className="hover:cursor-pointer   mx-auto select-none rounded-lg bg-gradient-to-tr from-[#327fa3] to-[#57a1b3] py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-                        >
-                          Manage
-                        </button>
-                      </Link>
+        <h1 className="m-2 sm:m-4 text-center">Email: {user?.email}</h1>
+        <div className="m-4 sm:m-10">
+          <table className="table w-full" {...getTableProps()}>
+            <thead>
+              {headerGroups.map((headerGroup) => (
+                <tr key="" {...headerGroup.getHeaderGroupProps()}>
+                  {headerGroup.headers.map((column) => (
+                    <th key="" {...column.getHeaderProps()}>
+                      {column.render("Header")}
                     </th>
+                  ))}
+                </tr>
+              ))}
+            </thead>
+            <tbody {...getTableBodyProps()}>
+              {rows.map((row) => {
+                prepareRow(row);
+                return (
+                  <tr key="" {...row.getRowProps()}>
+                    {row.cells.map((cell) => (
+                      <td key="" {...cell.getCellProps()}>
+                        {cell.render("Cell")}
+                      </td>
+                    ))}
                   </tr>
-                </tbody>
-              </table>
-            </div>
-          </div>
-        ))}
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
